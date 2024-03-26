b6e5efcd73db993e9416ad83b1a1e7274f89d7ac
SwiichyCode <adlpromail@gmail.com>
2024-03-26 14:39:01 +0100

    feat: implement lang selection

--- packages/configs/constants/urls/index.ts
+++ packages/configs/constants/urls/index.ts
@@ -1,5 +1,7 @@
 export const URL = {
   LANDING: "/",
+  LANDING_FR: "/fr",
+  LANDING_EN: "/en",
   LINKEDIN: "https://www.linkedin.com/in/swapnil-singh-1a1b3b1b3/",
   TWITTER: "https://github.com/SwiichyCode/GitShareSpace",
   GITHUB: "https://twitter.com/SwiichyCode_",
