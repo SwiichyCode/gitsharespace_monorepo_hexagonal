46c675d2db6808eb4a3f3c305c2298b3cdf73612
SwiichyCode <adlpromail@gmail.com>
2024-03-26 15:34:04 +0100

    build: setup sentry

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -9,6 +11,13 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     icon(),
+    sentry({
+      dsn: "https://bedf56ca8ec9f6b8965c678b62990ccc@o4506932955512832.ingest.us.sentry.io/4506932955643904",
+      sourceMapsUploadOptions: {
+        project: "gitsharespace_landing",
+        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
   ],
   i18n: {
     defaultLocale: "en",
